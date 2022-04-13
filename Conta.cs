c207188912ebec8ebcdecc409eab98206029a5ac
Ana Luisa Oliveira da Silva <analuisaoliveira714@gmail.com>
2022-04-13 11:19:41 -0300

    first commit

---
+++ Conta.cs
@@ -0,0 +1,52 @@
+public class Conta
+{ //se alterar outras propriedades ou receber mais de um parametro então eh um método
+    public int Codigo { get; }
+    public double Saldo { get; private set; }
+    
+    public Conta(int codigo)
+    {
+        Codigo = codigo;
+        Saldo = 0.0;
+    }
+
+    public void Sacar(double valor)
+    {
+        ValidarValor(valor);
+        VerificarSaldo(valor);
+        Saldo = Saldo - valor;
+    }
+
+    public void Depositar(double valor)
+    {
+        ValidarValor(valor);
+        Saldo = Saldo + valor;
+    }
+
+    public void Transferir (double valor, Conta conta)
+    {
+        // ValidarValor(valor);
+        // VerificarSaldo(valor);
+
+        // Saldo = Saldo - valor;
+        // conta.Saldo = conta.Saldo + valor;
+
+        Sacar (valor);
+        conta.Depositar (valor);
+    }
+
+    private void ValidarValor (double valor)
+    {
+        if (valor<0.0)
+        {
+            throw new ArgumentException("O valor é negativo");
+        }
+    }
+
+    private void VerificarSaldo (double valor)
+    {
+        if (valor>Saldo)
+        {
+            throw new ArgumentException("O saldo é insuficiente");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿Conta c1 = new Conta (1);
+Conta c2 = new Conta (2);
+
+c1.Depositar(1000.00);
+c1.Sacar(50.00);
+Console.WriteLine(c1.Codigo);
+Console.WriteLine(c1.Saldo);
+
+c2.Depositar(200.00);
+c2.Sacar(50.00);
+Console.WriteLine(c2.Codigo);
+Console.WriteLine(c2.Saldo);
+
+c2.Transferir(100.00, c1);
+Console.WriteLine("O novo saldo de c1 é:" + c1.Saldo);
+Console.WriteLine("O novo saldo de c2 é:" + c2.Saldo);
